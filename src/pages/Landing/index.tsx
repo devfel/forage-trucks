a5178c47a335710a67426adc049ff73d0b0f6da1
devfel <devfelizardo@gmail.com>
2022-01-04 22:33:46 -0500

    Fixing logo images and other info on the plataform design

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import logoImg from "../../assets/images/logo.svg";
+import logoImg from "../../assets/images/logo.png";
 import landingImg from "../../assets/images/landing.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
