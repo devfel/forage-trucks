ccbcc94030f03c0e8a1202f6f0c7f45af4d1ef24
devfel <devfelizardo@gmail.com>
2022-01-11 14:02:13 -0500

    chagning browserrouter to hashrouter

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ReservationList from "./pages/ReservationList";
 import ReserveCar from "./pages/ReserveCar";
 import Landing from "./pages/Landing";
 
 function MainRoutes() {
     return (
-        <BrowserRouter>
+        <HashRouter >
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/reserve" element={<ReserveCar />} />
                 <Route path="/reservation-list" element={<ReservationList />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter >
 
     );
 }
