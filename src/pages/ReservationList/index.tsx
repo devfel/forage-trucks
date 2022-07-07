a1a1e6fdfa9cd69c6880f07e8fb32eaabc7b9706
devfel <devfelizardo@gmail.com>
2022-07-07 15:42:36 -0400

    sorting reservation list by ID

--- src/pages/ReservationList/index.tsx
+++ src/pages/ReservationList/index.tsx
@@ -8,7 +8,7 @@ import "./styles.css"
 
 function ReservationList() {
 
-    const [reservationsList, setReservationsList] = useState([]);
+    const [reservationsList, setReservationsList] = useState<Array<Reservation>>([]);
     const [loading, setLoading] = useState(true);
 
     // Whenever the date changes this function searchs for Vehicles avaible on the DB
@@ -28,7 +28,7 @@ function ReservationList() {
             <main>
                 {loading ? (<div><img src={loading2} width="70px" alt="Loading" />{"Loading Data. Please Wait."}</div>) : null}
                 {
-                    reservationsList.slice(0).reverse().map((reservation: Reservation) => {
+                    reservationsList.sort(function (a, b) { return (a.id - b.id); }).slice(0).reverse().map((reservation: Reservation) => {
                         return <ReservationItem key={reservation.id} id={reservation.id} date={reservation.date} staff={reservation.staff} created_at={reservation.created_at} period={reservation.period} vehicle_id={reservation.vehicle_id} name={reservation.name} avatar={reservation.avatar} bio={reservation.bio} />
                     })
                 }
