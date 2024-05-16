da34394a2c842a7099921b29e50e11c43e23ef56
devfel <devfelizardo@gmail.com>
2024-05-15 22:25:14 -0300

    changing database and backend from heroku to adaptable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    //dev URL 
-    //baseURL: 'http://localhost:3333',
+  //dev URL
+  //baseURL: 'http://localhost:3333',
 
-    //Production URL
-    baseURL: 'https://forage-trucks-backend.herokuapp.com',
+  //Production URL 1
+  // baseURL: 'https://forage-trucks-backend.herokuapp.com',
+
+  //Production URL 2
+  baseURL: "https://forage-trucks-backend.adaptable.app",
 });
 
 export default api;
