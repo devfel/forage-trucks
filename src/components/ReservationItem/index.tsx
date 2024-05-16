39d1fccdc92376885766700f45f7aa4509cdb83c
devfel <devfelizardo@gmail.com>
2024-05-16 11:02:30 -0300

    adding remove reservation button

--- src/components/ReservationItem/index.tsx
+++ src/components/ReservationItem/index.tsx
@@ -1,41 +1,53 @@
 import React from "react";
 
-import "./styles.css"
+import "./styles.css";
 
 export interface Reservation {
-    id: number;
-    date: number;
-    staff: string;
-    created_at: string;
-    vehicle_id: number;
-    name: string;
-    avatar: string;
-    bio: string;
-    period: string;
+  id: number;
+  date: number;
+  staff: string;
+  created_at: string;
+  vehicle_id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  period: string;
 }
 
-const ReservationItem: React.FunctionComponent<Reservation> = ({ id, date, staff, created_at, vehicle_id, period, name, avatar, bio }) => {
-    const dReserved = new Date(+date).toUTCString().split(' ').slice(0, 4).join(' ');
-    const dCreated = new Date(created_at).toLocaleString();
-
-
-    return (
-        <article className="reservation-item">
-            <div className="text-information">
-                <header>
-                    <strong>{staff}</strong>
-                    <span><b>Date: {dReserved}</b></span>
-                    <span><b>Period: {period}</b></span>
-                    <span>Reservation Created at: {dCreated} GMT</span>
-                </header>
-
-                <p>Vehicle Name: {name}</p>
-                <p>Vehicle Description: {bio}</p>
-            </div>
-
-            <img src={`/forage-trucks/images/${avatar}.jpg`} alt={name} />
-        </article>
-    );
+interface ReservationItemProps extends Reservation {
+  onDelete: (id: number) => void;
 }
+
+const ReservationItem: React.FunctionComponent<ReservationItemProps> = ({ id, date, staff, created_at, vehicle_id, period, name, avatar, bio, onDelete }) => {
+  const dReserved = new Date(+date).toUTCString().split(" ").slice(0, 4).join(" ");
+  const dCreated = new Date(created_at).toLocaleString();
+
+  return (
+    <article className="reservation-item">
+      <div className="reservation-header">
+        <button className="delete-btn" onClick={() => onDelete(id)}>
+          X
+        </button>
+        <div className="text-information">
+          <header>
+            <strong>{staff}</strong>
+            <span>
+              <b>Date: {dReserved}</b>
+            </span>
+            <span>
+              <b>Period: {period}</b>
+            </span>
+            <span>Reservation Created at: {dCreated} GMT</span>
+          </header>
+
+          <p>Vehicle Name: {name}</p>
+          <p>Vehicle Description: {bio}</p>
+        </div>
+      </div>
+
+      <img src={`/forage-trucks/images/${avatar}.jpg`} alt={name} />
+    </article>
+  );
+};
 
 export default ReservationItem;
--- src/pages/ReservationList/index.tsx
+++ src/pages/ReservationList/index.tsx
@@ -4,37 +4,53 @@ import ReservationItem, { Reservation } from "../../components/ReservationItem";
 import api from "../../services/api";
 import loading2 from "../../assets/images/loading2.gif";
 
-import "./styles.css"
+import "./styles.css";
 
 function ReservationList() {
+  const [reservationsList, setReservationsList] = useState<Array<Reservation>>([]);
+  const [loading, setLoading] = useState(true);
 
-    const [reservationsList, setReservationsList] = useState<Array<Reservation>>([]);
-    const [loading, setLoading] = useState(true);
+  // Whenever the date changes this function searchs for Vehicles avaible on the DB
+  useEffect(() => {
+    api.get("reservations").then((response) => {
+      setLoading(true);
+      setReservationsList(response.data);
+      setLoading(false);
+    });
+  }, []);
 
-    // Whenever the date changes this function searchs for Vehicles avaible on the DB
-    useEffect(() => {
-        api.get('reservations')
-            .then(response => {
-                setLoading(true);
-                setReservationsList(response.data);
-                setLoading(false);
-            });
-    }, []);
+  const deleteReservation = (id: number) => {
+    setLoading(true);
+    api.delete(`reservations/${id}`).then(() => {
+      const updatedReservations = reservationsList.filter((reservation) => reservation.id !== id);
+      setReservationsList(updatedReservations);
+      setLoading(false);
+    });
+  };
 
-    return (
-        <div id="page-reservation-list" className="container">
-            <PageHeader title="List of all reservations done." />
+  return (
+    <div id="page-reservation-list" className="container">
+      <PageHeader title="List of all reservations done." />
 
-            <main>
-                {loading ? (<div><img src={loading2} width="70px" alt="Loading" />{"Loading Data. Please Wait."}</div>) : null}
-                {
-                    reservationsList.sort(function (a, b) { return (a.id - b.id); }).slice(0).reverse().map((reservation: Reservation) => {
-                        return <ReservationItem key={reservation.id} id={reservation.id} date={reservation.date} staff={reservation.staff} created_at={reservation.created_at} period={reservation.period} vehicle_id={reservation.vehicle_id} name={reservation.name} avatar={reservation.avatar} bio={reservation.bio} />
-                    })
-                }
-            </main>
-        </div>
-    )
+      <main>
+        {loading ? (
+          <div>
+            <img src={loading2} width="70px" alt="Loading" />
+            {"Loading Data. Please Wait."}
+          </div>
+        ) : null}
+        {reservationsList
+          .sort(function (a, b) {
+            return a.id - b.id;
+          })
+          .slice(0)
+          .reverse()
+          .map((reservation: Reservation) => {
+            return <ReservationItem key={reservation.id} {...reservation} onDelete={deleteReservation} />;
+          })}
+      </main>
+    </div>
+  );
 }
 
 export default ReservationList;
