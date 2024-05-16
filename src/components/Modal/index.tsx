ebcaaa152105b6f7d8d62b2c898fce90cc11b774
devfel <devfelizardo@gmail.com>
2024-05-16 13:21:21 -0300

    changing delete styling, adding modal for confirmation with passcode

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+
+import "./styles.css";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/ReservationItem/index.tsx
+++ src/components/ReservationItem/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal";
 
 import "./styles.css";
 
@@ -21,13 +22,39 @@ interface ReservationItemProps extends Reservation {
 const ReservationItem: React.FunctionComponent<ReservationItemProps> = ({ id, date, staff, created_at, vehicle_id, period, name, avatar, bio, onDelete }) => {
   const dReserved = new Date(+date).toUTCString().split(" ").slice(0, 4).join(" ");
   const dCreated = new Date(created_at).toLocaleString();
+  const [showModal, setShowModal] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleDelete = async () => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // Convert buffer to byte array
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join(""); // Convert bytes to hex string
+
+    const storedHash = "694d4f482f2a9ae1f64d1201f39ce86e1505137379236b5f99dc167e730c582a"; // Hash for "password"
+
+    if (hashHex === storedHash) {
+      onDelete(id);
+      setShowModal(false);
+    } else {
+      alert("Incorrect password!");
+    }
+  };
 
   return (
     <article className="reservation-item">
       <div className="reservation-header">
-        <button className="delete-btn" onClick={() => onDelete(id)}>
+        <button className="delete-btn" onClick={() => setShowModal(true)}>
           X
         </button>
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <h2>Confirm Deletion</h2>
+          <input className="input-password" type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button className="confirm-btn" onClick={handleDelete}>
+            Delete Reservation
+          </button>
+        </Modal>
         <div className="text-information">
           <header>
             <strong>{staff}</strong>
