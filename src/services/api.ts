a18a4186284b3cb4ef774ab854db526bfeb5c674
devfel <devfelizardo@gmail.com>
2024-11-04 14:13:21 -0300

    Adding the HTTPS axios api URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,7 +11,10 @@ const api = axios.create({
   // baseURL: "https://forage-trucks-backend.adaptable.app",
 
   //Production URL 3 - Amazon AWS EC2 Docker
-  baseURL: "http://54.87.16.232:3000",
+  // baseURL: "http://54.87.16.232:3000",
+
+  //Production URL 4 - Amazon AWS EC2 Docker - With NGINX and LEts Encrypt
+  baseURL: "https://foragetrucksbackend.devfel.com",
 });
 
 export default api;
